fb0e62bf66570a46264afc9c13ea2d02124dd789
Christian <christian.drumm@gmail.com>
2012-06-20 22:52:35 +0200

    Added a function to draw the maze

--- clr_programming_examples/src/maze_generation.clj
+++ clr_programming_examples/src/maze_generation.clj
@@ -23,3 +23,24 @@
   (set (concat
          (for [i (range (dec w)) j (range h)] #{[i j] [(inc i) j]})
          (for [i (range w) j (range (dec h))] #{[i j] [i (inc j)]}))))
+
+(defn draw  
+  [w h maze]
+  (doto (javax.swing.JFrame. "Maze")
+    (.setContentPane
+      (doto (proxy [javax.swing.JPanel] []
+              (paintComponent [^java.awt.Graphics g]
+                 (let [g (doto ^java.awt.Graphics2D (.create g)
+                           (.scale 10 10)
+                           (.translate 1.5 1.5)
+                           (.setStroke (java.awt.BasicStroke. 0.4)))]
+                   (.drawRect g -1 -1 w h)
+                   (doseq [[[xa ya] [xb yb]] (map sort maze)]
+                     (let [[xc yc] (if (= xa xb)
+                                     [(dec xa) ya]
+                                     [xa (dec ya)])]
+                       (.drawLine g xa ya xc yc))))))
+        (.setPreferredSize (java.awt.Dimension.
+                             (* 10 (inc w)) (* 10 (inc h))))))
+    .pack
+    (.setVisible true)))
